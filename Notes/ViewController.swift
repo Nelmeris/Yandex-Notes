062fe7a728ac9d2c0967377e9d134978fd1ae9cc
Nelmeris <kufaevartem@icloud.com>
2019-07-02 14:05:01 +0400

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Артем Куфаев on 02/07/2019.
+//  Copyright © 2019 Артем Куфаев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
